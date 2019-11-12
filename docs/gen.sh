243ba09c521500a7cb073f63604e36a52ce3ac30
Taco de Wolff <tacodewolff@gmail.com>
2019-11-12 16:29:55 -0300

    Update docs

--- docs/gen.sh
+++ docs/gen.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
-find * ! -name 'gen.sh' -exec rm -fr {} +
-pdoc --html --force -o . ../mogptk
-mv -f mogptk/* .
-rmdir mogptk
+find * ! -name 'gen.sh' -exec rm -fr {} + || exit 1
+pdoc --html --force -o . ../mogptk || exit 1
+mv -f mogptk/* . || exit 1
+rmdir mogptk || exit 1
