993e10c3de3fc17adc9e2cbc755023f8b70b6b37
Taco de Wolff <tacodewolff@gmail.com>
2019-12-19 15:47:56 -0300

    Add favicon.ico

--- docs/gen.sh
+++ docs/gen.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
-find * ! -name 'gen.sh' -exec rm -fr {} + || exit 1
+find * -type f -name '*.html' -exec rm -fr {} + || exit 1
 pdoc --html --force -o . ../mogptk || exit 1
 mv -f mogptk/* . || exit 1
 rmdir mogptk || exit 1
