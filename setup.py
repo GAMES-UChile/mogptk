9de8b19df2c6363b2d54b0425faf0ac180d0676c
Taco de Wolff <tacodewolff@gmail.com>
2020-01-28 17:15:27 -0300

    Update setup.py to use latest GPflow and TF 2.1; use JIT=false for optimizer to prevent errors

--- setup.py
+++ setup.py
@@ -1,4 +1,29 @@
+import sys
+from pkg_resources import parse_version
 from setuptools import setup, find_packages
+
+is_py37 = sys.version_info.major == 3 and sys.version_info.minor == 7
+
+requirements = [
+    'numpy>=1.18',
+    'gpflow @ git+https://github.com/GPflow/GPflow.git@develop#egg=gpflow',
+    'tensorflow-probability>=0.9',
+    'matplotlib',
+    'scipy',
+    'scikit-learn',
+    'pandas',
+    'seaborn',
+    'python-dateutil',
+    'ipython',
+    'dataclasses',
+] + ([] if is_py37 else ["dataclasses"])
+
+try:
+    import tensorflow as tf
+    if parse_version(tf.__version__) < parse_version('2.1.0'):
+        raise DeprecationWarning("TensorFlow version below minimum requirement")
+except (ImportError, DeprecationWarning):
+    requirements.append('tensorflow~=2.1')
 
 setup(name='mogptk',
       version='0.1',
@@ -7,21 +32,10 @@ setup(name='mogptk',
       author='Taco de Wolff, Alejandro Cuevas, Felipe Tobar',
       author_email='tacodewolff@gmail.com',
       license='MIT',
-      packages=find_packages(exclude=['examples']),
+      packages=find_packages('.', exclude=['examples']),
       include_package_data=False,
       keywords=['MOGP', 'MOSM', 'GP', 'Gaussian Process', 'Multi-Output', 'Tobar', 'Parra'],
-      python_requires='~=3.7.0',
-      install_requires=[
-          'numpy~=1.18.0',
-          'gpflow~=2.0.0rc1',
-          'tensorflow~=2.0.0',
-          'tensorflow-probability~=0.8.0',
-          'matplotlib',
-          'scipy',
-          'scikit-learn',
-          'pandas',
-          'seaborn',
-          'python-dateutil',
-          'ipython',
-      ],
+      python_requires='>=3.6',
+      extra_require={'TensorFlow with GPU': 'tensorflow-gpu'},
+      install_requires=requirements,
 )
