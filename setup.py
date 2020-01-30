e0fdaaf4e2b6428d8b617bb9c88b5e557d788575
Taco de Wolff <tacodewolff@gmail.com>
2020-01-30 17:45:22 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ is_py37 = sys.version_info.major == 3 and sys.version_info.minor == 7
 requirements = [
     'numpy>=1.10',
     'gpflow==2.0.0rc1',
-    'tensorflow-probability==0.8',
+    'tensorflow-probability>=0.8',
     'matplotlib',
     'scipy>=0.18',
     'scikit-learn',
@@ -24,7 +24,7 @@ try:
     if parse_version(tf.__version__) < parse_version('2.0.0'):
         raise DeprecationWarning("TensorFlow version below minimum requirement")
 except (ImportError, DeprecationWarning):
-    requirements.append('tensorflow==2.0')
+    requirements.append('tensorflow==2.1')
 
 this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
