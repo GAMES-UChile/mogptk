e34328b5ae65531ed312d6e4e9c67fe8fe082715
Taco de Wolff <tacodewolff@gmail.com>
2020-02-03 14:32:35 -0300

    Choose tensorflow-probabilities 0.8 or 0.9 depending on TF version

--- setup.py
+++ setup.py
@@ -8,7 +8,6 @@ is_py37 = sys.version_info.major == 3 and sys.version_info.minor == 7
 requirements = [
     'numpy>=1.10',
     'gpflow==2.0.0rc1',
-    'tensorflow-probability>=0.8',
     'matplotlib',
     'scipy>=0.18',
     'scikit-learn',
@@ -23,8 +22,13 @@ try:
     import tensorflow as tf
     if parse_version(tf.__version__) < parse_version('2.0.0'):
         raise DeprecationWarning("TensorFlow version below minimum requirement")
+    if parse_version(tf.__version__) < parse_version('2.1.0'):
+        requirements.append('tensorflow-probability==0.8')
+    else:
+        requirements.append('tensorflow-probability==0.9')
 except (ImportError, DeprecationWarning):
     requirements.append('tensorflow==2.1')
+    requirements.append('tensorflow-probability==0.9')
 
 this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
