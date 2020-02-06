428004f1f28c2fb22f8f91cae6dcaeb56322d79d
Taco de Wolff <tacodewolff@gmail.com>
2020-02-06 16:49:08 -0300

    Update requirements to remove tensorflow-probability

--- setup.py
+++ setup.py
@@ -1,13 +1,10 @@
-import sys
+from setuptools import setup
 from os import path
-from pkg_resources import parse_version
-from setuptools import setup, find_packages
-
-is_py37 = sys.version_info.major == 3 and sys.version_info.minor == 7
 
 requirements = [
     'numpy>=1.10',
     'gpflow==2.0.0rc1',
+    'tensorflow>=2.0',
     'matplotlib',
     'scipy>=0.18',
     'scikit-learn',
@@ -16,23 +13,7 @@ requirements = [
     'python-dateutil',
     'ipython',
     'dataclasses',
-] + ([] if is_py37 else ["dataclasses"])
-
-try:
-    import tensorflow as tf
-    if parse_version(tf.__version__) < parse_version('2.0.0'):
-        raise DeprecationWarning("TensorFlow version below minimum requirement")
-    if parse_version(tf.__version__) < parse_version('2.1.0'):
-        requirements.append('tensorflow-probability==0.8')
-    else:
-        requirements.append('tensorflow-probability==0.9')
-except (ImportError, DeprecationWarning):
-    if is_py37:
-        requirements.append('tensorflow==2.1')
-        requirements.append('tensorflow-probability==0.9')
-    else:
-        requirements.append('tensorflow==2.0')
-        requirements.append('tensorflow-probability==0.8')
+]
 
 this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
