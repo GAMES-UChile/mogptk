c468490493f7f7cd53ac0afae78a9383ec770674
Taco de Wolff <tacodewolff@gmail.com>
2020-02-06 12:46:08 -0300

    Add GitHub links and favicon to documentation

--- build_docs.sh
+++ build_docs.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 cd docs
 find * -type f -name '*.html' -exec rm -fr {} + || exit 1
-pdoc --html --force -o . ../mogptk || exit 1
+pdoc --html --template-dir .. --force -o . ../mogptk || exit 1
 mv -f mogptk/* . || exit 1
 rmdir mogptk || exit 1
