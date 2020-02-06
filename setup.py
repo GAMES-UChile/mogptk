5e5eeb8615cf1d21384b37695172024f11a061ef
Taco de Wolff <tacodewolff@gmail.com>
2020-02-06 12:22:48 -0300

    Release 0.1.3

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(name='mogptk',
-      version='0.1.2',
+      version='0.1.3',
       description='Multi-Output Gaussian Process ToolKit',
       long_description=long_description,
       long_description_content_type='text/markdown',
