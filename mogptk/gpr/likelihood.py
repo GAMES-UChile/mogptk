03347fae1b37c4736390351c02f536667cd61e54
Taco de Wolff <tacodewolff@gmail.com>
2021-11-29 11:35:04 -0300

    Add Bernoulli likelihood

--- mogptk/gpr/likelihood.py
+++ mogptk/gpr/likelihood.py
@@ -31,21 +31,23 @@ class Likelihood:
         q = self.quadrature(mu, var, lambda f: self.log_prob(y,f))  # Nx1
         return q.sum()  # sum over N
 
-    def mean(self, f):
-        # f: NxM
-        raise NotImplementedError()
+    #def mean(self, f):
+    #    # f: NxM
+    #    raise NotImplementedError()
 
-    def variance(self, f):
-        # f: NxM
-        raise NotImplementedError()
+    #def variance(self, f):
+    #    # f: NxM
+    #    raise NotImplementedError()
 
     def predict(self, mu, var, full=False):
+        # mu,var: Nx1
+        raise NotImplementedError()
         # ∫∫ y p(y|f) q(f) df dy,  ∫∫ y^2 p(y|f) q(f) df dy - (∫∫ y p(y|f) q(f) df dy)^2
         # where q(f) ~ N(mu,var) and p(y|f) is our likelihood
         # mu,var: Nx1
-        Ey = self.quadrature(mu, var, self.mean)
-        Eyy = self.quadrature(mu, var, lambda f: self.mean(f).square() + self.variance(f))
-        return Ey, Eyy-Ey**2 
+        #Ey = self.quadrature(mu, var, self.mean)
+        #Eyy = self.quadrature(mu, var, lambda f: self.mean(f).square() + self.variance(f))
+        #return Ey, Eyy-Ey**2 
 
 class GaussianLikelihood(Likelihood):
     def __init__(self, variance=1.0, name="GaussianLikelihood", quadratures=20):
@@ -66,15 +68,9 @@ class GaussianLikelihood(Likelihood):
         p -= self.sigma().log()
         return 0.5*p.sum()  # sum over N
 
-    def mean(self, f):
-        return f
-
-    def variance(self, f):
-        return torch.full(f.shape, self.sigma(), device=config.device, dtype=config.dtype)
-
     def predict(self, mu, var, full=False):
         if full:
-            return mu, var + self.sigma().diagflat()
+            return mu, var + self.sigma()*torch.eye(var.shape[0])
         else:
             return mu, var + self.sigma()
 
@@ -95,15 +91,15 @@ class StudentTLikelihood(Likelihood):
         p -= torch.log(self.scale())
         return p  # NxM
 
-    def mean(self, f):
-        return f
+    #def mean(self, f):
+    #    return f
 
-    def variance(self, f):
-        if self.dof < 2.0:
-            var = np.nan
-        else:
-            var = self.scale()**2 * self.dof/(self.dof-2.0)
-        return torch.full(f.shape, var, device=config.device, dtype=config.dtype)
+    #def variance(self, f):
+    #    if self.dof < 2.0:
+    #        var = np.nan
+    #    else:
+    #        var = self.scale()**2 * self.dof/(self.dof-2.0)
+    #    return torch.full(f.shape, var, device=config.device, dtype=config.dtype)
 
 class LaplaceLikelihood(Likelihood):
     def __init__(self, scale=1.0, name="LaplaceLikelihood", quadratures=20):
@@ -117,5 +113,32 @@ class LaplaceLikelihood(Likelihood):
         p = -torch.log(2.0*self.scale()) - (y-f).abs()/self.scale()
         return p  # NxM
 
+def inv_probit(x):
+    jitter = 1e-3
+    return 0.5*(1.0+torch.erf(x/np.sqrt(2.0))) * (1.0-2.0*jitter) + jitter
+
+def logistic(x):
+    return 1.0/(1.0+torch.exp(-x))
+
+class BernoulliLikelihood(Likelihood):
+    def __init__(self, scale=1.0, link=inv_probit, name="BernoulliLikelihood", quadratures=20):
+        super(BernoulliLikelihood, self).__init__(name, quadratures)
+
+        self.link = link
+
+    def log_prob(self, y, f):
+        # y: Nx1
+        # f: NxM
+        p = self.link(f)
+        return torch.log(torch.where(y == 1.0, p, 1.0-p))  # NxM
+
+    def predict(self, mu, var, full=False):
+        if self.link == inv_probit:
+            p = self.link(mu / torch.sqrt(1.0 + var))
+            return p, torch.zeros(var.shape)
+        else:
+            p = self.quadrature(mu, var, self.link)
+            return p, torch.zeros(var.shape)
+
 # TODO: implement log_prob: Beta, Softmax
-# TODO: implement log_prob and var_exp: Bernoulli, Gamma, Laplace, Poisson
+# TODO: implement log_prob and var_exp: Gamma, Laplace, Poisson
