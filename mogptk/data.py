Fix bug for noise=>variance, and change weight=>sigma for spectral kernel

mogptk/data.py
@@ -1005,7 +1005,7 @@ class Data:
         if plot:
             nyquist = self.get_nyquist_estimation()
             means = np.array([sm.model.kernel[0][q].mean.numpy() for q in range(Q)])
-            weights = np.array([sm.model.kernel[0][q].weight.numpy() for q in range(Q)])
+            weights = np.array([sm.model.kernel[0][q].sigma.numpy()**2 for q in range(Q)])
             scales = np.array([sm.model.kernel[0][q].variance.numpy() for q in range(Q)])
             nyquist = np.expand_dims(nyquist, 0)
             means = np.expand_dims(means, 1)
@@ -1013,7 +1013,7 @@ class Data:
             plot_spectrum(means, scales, weights=weights, nyquist=nyquist, title=self.name)
 
         for q in range(Q):
-            A[q,:] = sm.kernel[0][q].weight.numpy()  # TODO: weight is not per input_dims
+            A[q,:] = sm.kernel[0][q].sigma.numpy()**2  # TODO: weight is not per input_dims
             B[q,:] = sm.kernel[0][q].mean.numpy()
             C[q,:] = sm.kernel[0][q].variance.numpy()
         return A, B, C
@@ -1255,7 +1255,7 @@ def _check_function(f, input_dims, is_datetime64):
 
     x = [np.array([np.datetime64('2000', 'us')]) if is_datetime64[i] else np.ones((1,)) for i in range(input_dims)]
     y = f(*x)
-    if len(y.shape) != 1 or y.shape[0] != 1:
+    if y.ndim != 1 or y.shape[0] != 1:
         raise ValueError("function must return Y with shape (n,), note that all inputs are of shape (n,)")
 
 _datetime64_unit_names = {

mogptk/gpr/singleoutput.py
@@ -290,11 +290,11 @@ class SpectralKernel(Kernel):
     def __init__(self, input_dims=1, active_dims=None, name="SM"):
         super().__init__(input_dims, active_dims, name)
 
-        weight = torch.rand(1)
+        sigma = torch.rand(1)
         mean = torch.rand(input_dims)
         variance = torch.ones(input_dims)
 
-        self.weight = Parameter(weight, lower=config.positive_minimum)
+        self.sigma = Parameter(sigma, lower=config.positive_minimum)
         self.mean = Parameter(mean, lower=config.positive_minimum)
         self.variance = Parameter(variance, lower=config.positive_minimum)
 
@@ -304,12 +304,12 @@ class SpectralKernel(Kernel):
         tau = self.distance(X1,X2)  # NxMxD
         exp = -2.0*np.pi**2 * tau**2 * self.variance().reshape(1,1,-1)  # NxMxD
         cos = 2.0*np.pi * tau * self.mean().reshape(1,1,-1)  # NxMxD
-        return self.weight() * torch.prod(torch.exp(exp) * torch.cos(cos), dim=2)
+        return self.sigma()**2 * torch.prod(torch.exp(exp) * torch.cos(cos), dim=2)
 
     def K_diag(self, X1):
         # X has shape (data_points,input_dims)
         X1, _ = self._active_input(X1)
-        return self.weight() * torch.ones(X1.shape[0], dtype=config.dtype, device=config.device)
+        return self.sigma()**2 * torch.ones(X1.shape[0], dtype=config.dtype, device=config.device)
 
 class MaternKernel(Kernel):
     def __init__(self, nu=0.5, input_dims=1, active_dims=None, name="Matérn"):

mogptk/models/conv.py
@@ -106,9 +106,10 @@ class CONV(Model):
         for q in range(self.Q):
             self.model.kernel[q].weight.assign(constant[:,q])
 
-        noise = np.empty((output_dims,))
-        for i, channel in enumerate(self.dataset):
-            _, y = channel.get_train_data(transformed=True)
-            noise[i] = y.var() / 30.0
-        for q in range(self.Q):
-            self.model.kernel[q].noise.assign(noise)
+        # TODO
+        #noise = np.empty((output_dims,))
+        #for i, channel in enumerate(self.dataset):
+        #    _, y = channel.get_train_data(transformed=True)
+        #    noise[i] = y.var() / 30.0
+        #for q in range(self.Q):
+        #    self.model.kernel[q].noise.assign(noise)

mogptk/models/mosm.py
@@ -113,12 +113,13 @@ class MOSM(Model):
         for q in range(self.Q):
             self.model.kernel[q].magnitude.assign(magnitude[:,q])
 
-        noise = np.empty((output_dims,))
-        for j, channel in enumerate(self.dataset):
-            _, y = channel.get_train_data(transformed=True)
-            noise[j] = y.var() / 30.0
-        for q in range(self.Q):
-            self.model.kernel[q].noise.assign(noise)
+        # TODO
+        #noise = np.empty((output_dims,))
+        #for j, channel in enumerate(self.dataset):
+        #    _, y = channel.get_train_data(transformed=True)
+        #    noise[j] = y.var() / 30.0
+        #for q in range(self.Q):
+        #    self.model.kernel[q].noise.assign(noise)
 
     def check(self):
         """

mogptk/models/sm.py
@@ -88,7 +88,7 @@ class SM(Model):
                 variances = 1.0 / (np.abs(np.random.randn(self.Q, input_dims[j])) * x_range)
 
                 for q in range(self.Q):
-                    self.model.kernel[j][q].weight.assign(weights[q])
+                    self.model.kernel[j][q].sigma.assign(np.sqrt(weights[q]))
                     self.model.kernel[j][q].mean.assign(means[q,:])
                     self.model.kernel[j][q].variance.assign(variances[q,:])
             return
@@ -131,7 +131,7 @@ class SM(Model):
                 mixture_weights /= amplitudes[j].sum()
             mixture_weights *= 2.0 * y.std()
             for q in range(self.Q):
-                self.model.kernel[j][q].weight.assign(mixture_weights[q])
+                self.model.kernel[j][q].sigma.assign(np.sqrt(mixture_weights[q]))
                 self.model.kernel[j][q].mean.assign(means[j][q,:])
                 self.model.kernel[j][q].variance.assign(variances[j][q,:])
 
@@ -145,6 +145,6 @@ class SM(Model):
 
         means = np.array([[self.model.kernel[j][q].mean.numpy() for j in range(output_dims)] for q in range(self.Q)])
         scales = np.array([[self.model.kernel[j][q].variance.numpy() for j in range(output_dims)] for q in range(self.Q)])
-        weights = np.array([[self.model.kernel[j][q].weight.numpy()[0] for j in range(output_dims)] for q in range(self.Q)])
+        weights = np.array([[self.model.kernel[j][q].sigma.numpy()[0]**2 for j in range(output_dims)] for q in range(self.Q)])
 
         return plot_spectrum(means, scales, weights=weights, nyquist=nyquist, titles=names, title=title)

mogptk/models/sm_lmc.py
@@ -118,8 +118,9 @@ class SM_LMC(Model):
                 constant[i,:,:] = constant[i,:,:] / constant[i,:,:].sum() * y.var() * 2
         self.model.kernel.weight.assign(constant)
 
-        noise = np.empty((output_dims,))
-        for i, channel in enumerate(self.dataset):
-            _, y = channel.get_train_data(transformed=True)
-            noise[i] = y.var() / 30.0
-        self.model.kernel.noise.assign(noise)
+        # TODO
+        #noise = np.empty((output_dims,))
+        #for i, channel in enumerate(self.dataset):
+        #    _, y = channel.get_train_data(transformed=True)
+        #    noise[i] = y.var() / 30.0
+        #self.model.kernel.noise.assign(noise)