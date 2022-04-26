de02707729d0d7412e2a9e49f34676acd139913e
Taco de Wolff <tacodewolff@gmail.com>
2022-04-26 09:43:55 -0400

    Fix bug

--- mogptk/gpr/kernel.py
+++ mogptk/gpr/kernel.py
@@ -388,7 +388,7 @@ class MultiOutputKernel(Kernel):
         X1, X2 = super()._check_input(X1, X2)
         if not torch.all(X1[:,0] == X1[:,0].long().type(config.dtype)):
             raise ValueError("X must have integers for the channel IDs in the first input dimension")
-        if not torch.all(X2[:,0] == X2[:,0].long().type(config.dtype)):
+        if X2 is not None and not torch.all(X2[:,0] == X2[:,0].long().type(config.dtype)):
             raise ValueError("X must have integers for the channel IDs in the first input dimension")
         return X1, X2
 
