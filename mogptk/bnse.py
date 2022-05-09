cdbaf3821d8bb16bac7451257cee6db57b58bcb2
Taco de Wolff <tacodewolff@gmail.com>
2022-05-09 17:34:01 -0400

    Fixes for BNSE

--- mogptk/bnse.py
+++ mogptk/bnse.py
@@ -25,10 +25,10 @@ def BNSE(x, y, max_freq=None, n=1000, iters=500, **params):
     mean = 0.01
     variance = 0.25 / np.pi**2 / x_dist**2
     noise = y.std()/10.0
-    kernel.sigma.assign(sigma)
-    kernel.mean.assign(mean, upper=max_freq)
-    kernel.variance.assign(variance)
-    model.likelihood.variance.assign(noise)
+    model.kernel.sigma.assign(sigma)
+    model.kernel.mean.assign(mean, upper=max_freq)
+    model.kernel.variance.assign(variance)
+    model.likelihood.scale.assign(noise)
 
     # train model
     optimizer = torch.optim.LBFGS(model.parameters(), max_iter=iters)
@@ -37,7 +37,7 @@ def BNSE(x, y, max_freq=None, n=1000, iters=500, **params):
     #for i in range(iters):
     #    loss = optimizer.step(model.loss)
 
-    alpha = 2.0/x_range**2  # TODO: divide by 4?
+    alpha = 0.5/x_range**2
     w = torch.linspace(0.0, max_freq, n, device=gpr.config.device, dtype=gpr.config.dtype).reshape(-1,1)
 
     def kernel_ff(f1, f2, sigma, mean, variance, alpha):
@@ -47,9 +47,8 @@ def BNSE(x, y, max_freq=None, n=1000, iters=500, **params):
         variance = variance.reshape(1,1,-1)
         exp1 = -0.5 * np.pi**2 / alpha * gpr.Kernel.squared_distance(f1,f2)  # MxMxD
 
-        # TODO: change to np.pi**2
-        exp2a = -2.0 * np.pi*2 / (alpha+4.0*np.pi**2*variance) * (gpr.Kernel.average(f1,f2)-mean)**2  # MxMxD
-        exp2b = -2.0 * np.pi*2 / (alpha+4.0*np.pi**2*variance) * (gpr.Kernel.average(f1,f2)+mean)**2  # MxMxD
+        exp2a = -2.0 * np.pi**2 / (alpha+4.0*np.pi**2*variance) * (gpr.Kernel.average(f1,f2)-mean)**2  # MxMxD
+        exp2b = -2.0 * np.pi**2 / (alpha+4.0*np.pi**2*variance) * (gpr.Kernel.average(f1,f2)+mean)**2  # MxMxD
         return const * (torch.exp(exp1+exp2a) + torch.exp(exp1+exp2b)).sum(dim=2)
 
     def kernel_tf(t, f, sigma, mean, variance, alpha):
@@ -58,22 +57,23 @@ def BNSE(x, y, max_freq=None, n=1000, iters=500, **params):
         variance = variance.reshape(1,-1)
         gamma = 2.0*np.pi**2*variance
         Lq_inv = np.pi**2 * (1.0/alpha + 1.0/gamma)  # 1xD
-        Lq_inv = 1.0/Lq_inv # TODO: remove line
+        Lq_inv = 1.0/Lq_inv  # TODO: this line must be kept, is this wrong in the paper?
 
-        a = 0.5 * torch.sqrt(np.pi/(alpha+gamma.prod()))  # 1
+        const = torch.sqrt(np.pi/(alpha+gamma.prod()))  # 1
         exp1 = -np.pi**2 * torch.tensordot(t**2, Lq_inv.T, dims=1)  # Nx1
-        exp2a = -np.pi**2 * torch.tensordot(1.0/(alpha+gamma), (f-mean).T**2, dims=1)  # 1xM
-        exp2b = -np.pi**2 * torch.tensordot(1.0/(alpha+gamma), (f+mean).T**2, dims=1)  # 1xM
+        exp2a = -torch.tensordot(np.pi**2/(alpha+gamma), (f-mean).T**2, dims=1)  # 1xM
+        exp2b = -torch.tensordot(np.pi**2/(alpha+gamma), (f+mean).T**2, dims=1)  # 1xM
         exp3a = -2.0*np.pi * torch.tensordot(t.mm(Lq_inv), np.pi**2 * (f/alpha + mean/gamma).T, dims=1)  # NxM
         exp3b = -2.0*np.pi * torch.tensordot(t.mm(Lq_inv), np.pi**2 * (f/alpha - mean/gamma).T, dims=1)  # NxM
 
+        a = 0.5 * sigma**2 * const * torch.exp(exp1)
         real = torch.exp(exp2a)*torch.cos(exp3a) + torch.exp(exp2b)*torch.cos(exp3b)
         imag = torch.exp(exp2a)*torch.sin(exp3a) + torch.exp(exp2b)*torch.sin(exp3b)
-        return sigma**2 * a * torch.exp(exp1) * real, sigma**2 * a * torch.exp(exp1) * imag
+        return a * real, a * imag
 
     with torch.no_grad():
         Ktt = kernel(x)
-        Ktt += model.likelihood.variance() * torch.eye(x.shape[0], device=gpr.config.device, dtype=gpr.config.dtype)
+        Ktt += model.likelihood.scale() * torch.eye(x.shape[0], device=gpr.config.device, dtype=gpr.config.dtype)
         Ltt = model._cholesky(Ktt, add_jitter=True)
 
         Kff = kernel_ff(w, w, kernel.sigma(), kernel.mean(), kernel.variance(), alpha)
@@ -92,7 +92,7 @@ def BNSE(x, y, max_freq=None, n=1000, iters=500, **params):
         var_real = Kff_real - b.T.mm(b)
         var_imag = Kff_imag - c.T.mm(c)
 
-        psd = mu_real**2 + mu_imag**2 + (var_real + var_real).diagonal().reshape(-1,1)  # TODO: use var_imag?
+        psd = mu_real**2 + mu_imag**2 + (var_real + var_imag).diagonal().reshape(-1,1)
         w = w.cpu().numpy()
         psd = psd.cpu().numpy()
     return w, psd
