Fix peak variance/amplitude estimation

mogptk/data.py
@@ -853,13 +853,15 @@ class Data:
         if len(peaks) == 0:
             return np.array([]), np.array([]), np.array([])
         peaks = peaks[np.argsort(psd[peaks])[::-1]] # sort by biggest peak first
+        peaks = peaks[0.0 < psd[peaks]] # filter out negative peaks which sometimes occur
 
         widths, _, _, _ = signal.peak_widths(psd, peaks, rel_height=0.5)
         widths *= w[1]-w[0]
 
         positions = w[peaks]
-        variances = widths / (8.0*np.log(2.0)) # from full-width half-maximum to Gaussian sigma
+        variances = widths**2 / (8.0*np.log(2.0)) # from full-width half-maximum to Gaussian sigma
         amplitudes = psd[peaks] * np.sqrt(2.0*np.pi*variances)
+        amplitudes = np.sqrt(amplitudes)
         return amplitudes, positions, variances
 
     def get_ls_estimation(self, Q=1, n=10000):
@@ -887,7 +889,8 @@ class Data:
         x, y = self.get_train_data(transformed=True)
         for i in range(input_dims):
             w = np.linspace(0.0, nyquist[i], n+1)[1:]
-            psd = signal.lombscargle(x[:,i]*2.0*np.pi, y, w, normalize=True)
+            psd = signal.lombscargle(x[:,i]*2.0*np.pi, y, w)
+            psd /= 2.0*np.pi
             amplitudes, positions, variances = self._get_psd_peaks(w, psd)
             if len(positions) == 0:
                 continue
@@ -928,6 +931,7 @@ class Data:
         x, y = self.get_train_data(transformed=True)
         for i in range(input_dims):
             w, psd, _ = BNSE(x[:,i], y, max_freq=nyquist[i], n=n, iters=iters)
+            psd /= np.sqrt(2) # TODO: why? emperically found
             amplitudes, positions, variances = self._get_psd_peaks(w, psd)
             if len(positions) == 0:
                 continue
@@ -1132,7 +1136,7 @@ class Data:
         Y_freq_err = np.array([])
         if method.lower() == 'ls':
             X_freq = np.linspace(0.0, nyquist, n)[1:]
-            Y_freq = signal.lombscargle(X*2.0*np.pi, Y, X_freq, normalize=True)
+            Y_freq = signal.lombscargle(X*2.0*np.pi, Y, X_freq)
         elif method.lower() == 'bnse':
             X_freq, Y_freq, Y_freq_err = BNSE(X, Y, max_freq=nyquist, n=n)
         else: